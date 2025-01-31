2952997d670840c3b2cb5af7cdf588e0fa276fef
ITBila-Coder23 <bilathabiso@gmail.com>
2025-01-31 02:18:40 +0200

    Update Flask Web App.py

--- Flask Web App.py
+++ Flask Web App.py
@@ -1,1 +1,22 @@
-Flask Web App.py
+ci-cd-flask-app/
+│── app/
+│   │── app.py
+│   │── requirements.txt
+│   │── Dockerfile
+│── k8s/
+│   │── deployment.yaml
+│   │── service.yaml
+│── .github/workflows/
+│   │── ci-cd-pipeline.yml
+│── README.md
+  
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello, Cloud & DevOps! Your app is running on Kubernetes 🎉"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+  
